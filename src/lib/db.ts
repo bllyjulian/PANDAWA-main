97bf74768cdd5df54dde2095d9c50649bd579f8f
bllyjulian <billyjulian75@gmail.com>
2026-04-04 16:09:11 +0700

    update : db online

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,13 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_pandawa',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 4000,
+  // Opsi SSL di bawah ini WAJIB ada agar bisa nyambung ke TiDB
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
-  
